af7ec8b190ba2558820c0e1dc35b026aa316259c
Elliott Minns <elliott.minns@pm.me>
2023-01-07 14:07:02 -0600

    update documentation

--- app/option.go
+++ app/option.go
@@ -4,7 +4,9 @@ package app
 // options are typically only meant for internal testing.
 type Option func(a *App)
 
-// WithIDGenerator overrides the internal ID generator of the app.
+// WithIDGenerator overrides the internal ID generator of the app. Use this
+// method for testing or for use with an exchange that does not support the
+// id format in use by the app.
 func WithIDGenerator(gen IDGenerator) Option {
 	return func(a *App) {
 		a.idGenerator = gen
